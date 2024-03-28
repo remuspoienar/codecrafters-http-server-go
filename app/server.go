44d30e307f74acd1dd7bd237077ff3daf6a8ca67
Remus Poienar <remus.e.poienar@gmail.com>
2024-03-28 23:35:56 +0200

    pass stage 6

--- app/server.go
+++ app/server.go
@@ -12,9 +12,58 @@ import (
 
 const GET = "GET"
 
+func handleConn(conn net.Conn) {
+	defer conn.Close()
+
+	scanner := bufio.NewScanner(conn)
+	scanner.Split(bufio.ScanLines)
+
+	scanner.Scan()
+	head := scanner.Text()
+
+	if head == "" {
+		fmt.Println("Error parsing connection: Empty request data")
+		return
+	}
+
+	parts := strings.Split(head, " ")
+	verb, path := parts[0], parts[1]
+
+	if verb == GET && strings.Contains(path, "/user-agent") {
+		var ua string
+
+		for scanner.Scan() {
+			line := scanner.Text()
+			if line == "" {
+				break
+			}
+			if strings.Contains(line, "User-Agent:") {
+				ua = strings.Split(line, "User-Agent: ")[1]
+				break
+			}
+		}
+
+		fmt.Fprintf(conn, "HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: %d\r\n\r\n%s", len(ua), ua)
+		return
+	}
+
+	if verb == GET && strings.Contains(path, "/echo/") {
+		splitPath := strings.Split(path, "echo/")
+		text := splitPath[len(splitPath)-1:][0]
+		fmt.Fprintf(conn, "HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: %d\r\n\r\n%s", len(text), text)
+
+		return
+	}
+
+	if path == "/" {
+		conn.Write([]byte("HTTP/1.1 200 OK\r\n\r\n"))
+		return
+	}
+
+	conn.Write([]byte("HTTP/1.1 404 Not Found\r\n\r\n"))
+
+}
 func main() {
-	// You can use print statements as follows for debugging, they'll be visible when running tests.
-	fmt.Println("Logs from your program will appear here!")
 
 	l, err := net.Listen("tcp", "0.0.0.0:4221")
 	if err != nil {
@@ -29,56 +78,7 @@ func main() {
 			os.Exit(1)
 		}
 
-		scanner := bufio.NewScanner(conn)
-		scanner.Split(bufio.ScanLines)
-
-		scanner.Scan()
-		head := scanner.Text()
-
-		if err != nil {
-			fmt.Println("Error parsing connection: ", err.Error())
-			continue
-		}
-
-		parts := strings.Split(head, " ")
-		verb, path := parts[0], parts[1]
-
-		if verb == GET && strings.Contains(path, "/user-agent") {
-			var ua string
-
-			for scanner.Scan() {
-				line := scanner.Text()
-				if line == "" {
-					break
-				}
-				if strings.Contains(line, "User-Agent:") {
-					ua = strings.Split(line, "User-Agent: ")[1]
-					break
-				}
-			}
-
-			fmt.Fprintf(conn, "HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: %d\r\n\r\n%s", len(ua), ua)
-			conn.Close()
-			continue
-		}
-
-		if verb == GET && strings.Contains(path, "/echo/") {
-			splitPath := strings.Split(path, "echo/")
-			text := splitPath[len(splitPath)-1:][0]
-			fmt.Fprintf(conn, "HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: %d\r\n\r\n%s", len(text), text)
-			conn.Close()
-			continue
-		}
-
-		if path != "/" {
-			conn.Write([]byte("HTTP/1.1 404 Not Found\r\n\r\n"))
-			conn.Close()
-			continue
-		}
-
-		conn.Write([]byte("HTTP/1.1 200 OK\r\n\r\n"))
-		conn.Close()
-
+		go handleConn(conn)
 	}
 
 }
