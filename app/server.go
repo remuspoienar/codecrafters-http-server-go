af1196b6d19bd43da8bff6e0330ab0d5894f0566
Remus Poienar <11542010+remuspoienar@users.noreply.github.com>
2024-06-04 00:20:30 +0300

    codecrafters submit [skip ci]

--- app/server.go
+++ app/server.go
@@ -9,6 +9,7 @@ import (
 	"net"
 	"net/http"
 	"os"
+	"regexp"
 	"strings"
 )
 
@@ -35,13 +36,20 @@ func handleConn(conn net.Conn) {
 
 	verb, pathObj := request.Method, request.URL
 	path := pathObj.Path
-	encoding := request.Header["Accept-Encoding"]
-	if len(encoding) == 0 {
-		encoding = request.Header["accept-encoding"]
+
+	encodings := strings.Split(request.Header.Get("Accept-Encoding"), ",")
+	if len(encodings) == 0 {
+		encodings = strings.Split(request.Header.Get("accept-encoding"), ",")
 	}
 
-	if len(encoding) > 0 && !strings.Contains("gzip compress deflate br zstd identity *", encoding[0]) {
-		encoding = []string{}
+	var validEncodings []string
+	validValueRegexp := regexp.MustCompile(`^(gzip|compress|deflate|br|zstd|identity|\*)$`)
+
+	for _, encoding := range encodings {
+		enc := strings.TrimSpace(encoding)
+		if validValueRegexp.MatchString(enc) {
+			validEncodings = append(validEncodings, enc)
+		}
 	}
 
 	if strings.Contains(path, "/files/") {
@@ -77,8 +85,10 @@ func handleConn(conn net.Conn) {
 		text := splitPath[len(splitPath)-1:][0]
 
 		respEncoding := ""
-		if len(encoding) > 0 {
-			respEncoding = fmt.Sprintf("Content-Encoding: %s\r\n", strings.Join(encoding, ", "))
+		if len(validEncodings) > 0 {
+			sendEncoding := strings.Join(validEncodings, ", ")
+
+			respEncoding = fmt.Sprintf("Content-Encoding: %s\r\n", sendEncoding)
 		}
 		fmt.Fprintf(conn, "HTTP/1.1 200 OK\r\n%sContent-Type: text/plain\r\nContent-Length: %d\r\n\r\n%s", respEncoding, len(text), text)
 
